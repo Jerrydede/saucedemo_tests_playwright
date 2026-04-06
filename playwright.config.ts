5f1e4a8e808d0834cec1a66ec964aeff88c1f050
Jerry Liu <lqlsky@gmail.com>
2026-04-06 18:47:52 +0200

    Added screenshot: 'only-on-failure'

--- playwright.config.ts
+++ playwright.config.ts
@@ -31,6 +31,8 @@ export default defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     baseURL: 'https://www.saucedemo.com/', 
     trace: 'on-first-retry',
+    screenshot: 'only-on-failure',
+    //video: 'retain-on-failure',
   },
 
   /* Configure projects for major browsers */
@@ -55,10 +57,10 @@ export default defineConfig({
       name: 'Mobile Chrome',
       use: { ...devices['Pixel 5'] },
     },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
+    {
+      name: 'Mobile Safari',
+      use: { ...devices['iPhone 12'] },
+    },
 
     /* Test against branded browsers. */
     // {
